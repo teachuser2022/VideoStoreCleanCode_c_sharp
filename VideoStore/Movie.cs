19c310430d208c99588b92b1d2d168247ae3eecc
Milad Ahmadi <69338883+ShowAppStructure@users.noreply.github.com>
2022-08-02 21:23:26 -0700

    Initial commit

---
+++ VideoStore/Movie.cs
@@ -0,0 +1,17 @@
+namespace TestVideoStore;
+
+public class Movie
+{
+    public const int REGULAR     = 0;
+    public const int NEW_RELEASE = 1;
+    public const int CHILDREN    = 2;
+
+    public int PriceCode { get; set; }
+    public virtual string Title { get; }
+
+    public Movie(string title, int priceCode)
+    {
+        Title     = title;
+        PriceCode = priceCode;
+    }
+}
---
+++ VideoStore/Rental.cs
@@ -0,0 +1,13 @@
+namespace TestVideoStore;
+
+public class Rental
+{
+    public int DaysRented { get; }
+    public virtual Movie Movie { get; }
+
+    public Rental(Movie movie, int daysRented)
+    {
+        Movie      = movie;
+        DaysRented = daysRented;
+    }
+}
